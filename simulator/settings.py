cc86168a5a94a6ec1164d90888d8e917f469a7a0
FreddyWordingham <freddy@digilab.co.uk>
2023-03-13 02:22:23 +0000

    Moved TIME_STEP_MS setting to .env file

--- simulator/settings.py
+++ simulator/settings.py
@@ -5,6 +5,7 @@ import os
 
 class Environment(BaseSettings):
     RESOURCES_DIR: str
+    TIME_STEP_MS: int
 
     class Config:
         env_prefix = ""
@@ -18,5 +19,5 @@ SCENARIO_DIR = os.path.join(ENV.RESOURCES_DIR, "scenarios")
 
 TIME_FORMAT = "%Y-%m-%d %H:%M:%S"  # Format for datetime strings in input files.
 TIME_STEP_DELTA = datetime.timedelta(
-    milliseconds=125
+    milliseconds=ENV.TIME_STEP_MS
 )  # Steps will always be this amount of time. Longer steps will be broken down into multiple steps of this duration.
